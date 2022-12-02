b3d40b9076da63f836f00ba1c4ff873056a952c7
Peter Lee <PeterLee2094@gmail.com>
2022-12-01 20:06:20 -0600

    finsihed project

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,60 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  $('#submit').on('click', submit);
+  $('#change-num').on('click', changeNum)
+}
+let rounds = 1;
+function submit(){
+  console.log($('#input-1').val());
+  console.log($('#input-2').val());
+  
+  rounds ++;
+  $('#rounds').text(`guess #${rounds}`)
+  let guesses = {
+    guess1 : $('#input-1').val(),
+    guess2 : $('#input-2').val()
+  };
+
+  $.ajax({
+    method: 'POST',
+    url:'/',
+    data: guesses
+  }).then(function(response){
+    console.log(response);
+    renderUpdate(response)
+  }).catch(function(response){
+    console.log('error, ', response);
+  });
+}
+
+function renderUpdate(data){
+  for(let i = 0; i<data.length; i++){
+    if(data[i].update == 'correct'){
+      $(`
+        <h1> ${data[i].guesser} is the WINNER !!! </h1>
+      `).appendTo('#screen-winner');
+    }else{
+      $(`
+        <h1> ${data[i].guesser} is ${data[i].update} than the correct number </h1>
+      `).appendTo('#history');
+    }
+    
+  }
+  
+}
+
+
+function changeNum(){
+  let newNum = {num:Math.floor(Math.random() * (1 + 25 - 1) + 1)}
+
+  $.ajax({
+    method:"POST",
+    url:'/generate',
+    data: newNum
+  }).then(function(response){
+    console.log('the new number has been generated');
+  }).catch(function(response){
+    console.log('failed to generate new number, ', response)
+  })
 }
--- server/server.js
+++ server/server.js
@@ -9,7 +9,80 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+// number need to be guessed
+let random = randomNumber(1, 25)
+
+// comparison function
+
+
 // GET & POST Routes go here
+app.post('/', (req, res)=>{
+  console.log(req.body);
+  let arr = [];
+
+  if(req.body.guess1 == random){
+    console.log('correct');
+    arr.push({
+      update:"correct",
+      guesser: 'guesser1'
+    })
+  }
+  else if(req.body.guess2 == random){
+    console.log('correct');
+    arr.push({
+      update:"correct",
+      guesser: 'guesser2'
+    })
+
+  }else{
+    console.log('incorrect')
+    if(req.body.guess1 > random){
+      console.log('your guess is greater')
+      arr.push({
+        update:"higher",
+        guesser: 'guesser1'
+      })
+    }
+    else{
+      console.log('your guess is lower')
+      arr.push({
+        update:"lower",
+        guesser: 'guesser1'
+      })
+    }
+
+    if(req.body.guess2 > random){
+      console.log('your guess is greater')
+      arr.push({
+        update:"higher",
+        guesser: 'guesser2'
+      })
+    }
+    else{
+      console.log('your guess is lower');
+      arr.push({
+        update:"lower",
+        guesser: 'guesser2'
+      })
+    }
+
+  }
+
+  app.post('/generate', (req, res)=>{
+    random = req.body.num;
+    console.log(random);
+  })
+  console.log(random);
+  console.log(arr);
+  // ['guess1' ,'guess2']
+
+  res.send(arr);
+});
 
 
 app.listen(PORT, () => {
